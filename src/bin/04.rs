11fa0bc177116b8772110c43e3a7bb0d6d3c358a
noook <me@neilrichter.com>
2025-08-22 11:47:18 +0200

    feat: implement passport parsing and validation for Day 4

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -1,7 +1,71 @@
+use std::fmt::{Debug};
+
 advent_of_code::solution!(4);
 
-pub fn part_one(_input: &str) -> Option<u64> {
-    None
+#[derive(Debug)]
+struct Passport {
+    byr: Option<String>,
+    iyr: Option<String>,
+    eyr: Option<String>,
+    hgt: Option<String>,
+    hcl: Option<String>,
+    ecl: Option<String>,
+    pid: Option<String>,
+    cid: Option<String>,
+}
+
+fn parse_input(input: &str) -> impl Iterator<Item = Passport> + '_ {
+    input
+        .trim()
+        .split("\n\n")
+        .map(|s| s.replace("\n", " "))
+        .map(|s| {
+            let mut passport = Passport {
+                byr: None,
+                iyr: None,
+                eyr: None,
+                hgt: None,
+                hcl: None,
+                ecl: None,
+                pid: None,
+                cid: None,
+            };
+            for field in s.split_whitespace() {
+                if let Some((key, value)) = field.split_once(':') {
+                    match key {
+                        "byr" => passport.byr = Some(value.to_string()),
+                        "iyr" => passport.iyr = Some(value.to_string()),
+                        "eyr" => passport.eyr = Some(value.to_string()),
+                        "hgt" => passport.hgt = Some(value.to_string()),
+                        "hcl" => passport.hcl = Some(value.to_string()),
+                        "ecl" => passport.ecl = Some(value.to_string()),
+                        "pid" => passport.pid = Some(value.to_string()),
+                        "cid" => passport.cid = Some(value.to_string()),
+                        _ => {}
+                    }
+                }
+            }
+            passport
+        })
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let lines: Vec<Passport> = parse_input(input).filter(|passport| {
+        match passport {
+            Passport {
+                byr: Some(_),
+                iyr: Some(_),
+                eyr: Some(_),
+                hgt: Some(_),
+                hcl: Some(_),
+                ecl: Some(_),
+                pid: Some(_),
+                cid: _,
+            } => true,
+            _ => false,
+        }
+    }).collect();
+    Some(lines.len() as u64)
 }
 
 pub fn part_two(_input: &str) -> Option<u64> {
@@ -15,7 +79,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(2));
     }
 
     #[test]
