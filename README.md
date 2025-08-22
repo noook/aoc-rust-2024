b278ff5df661e1414dcd7302c602dc6b071125cc
noook <me@neilrichter.com>
2025-08-22 11:06:47 +0200

    fix: update Day 2 benchmarks and add Day 3 results; refine test result types in Day 1

--- README.md
+++ README.md
@@ -12,9 +12,10 @@ Solutions for [Advent of Code](https://adventofcode.com/) in [Rust](https://www.
 | Day | Part 1 | Part 2 |
 | :---: | :---: | :---:  |
 | [Day 1](./src/bin/01.rs) | `4.1µs` | `40.9µs` |
-| [Day 2](./src/bin/02.rs) | `408.7µs` | `402.3µs` |
+| [Day 2](./src/bin/02.rs) | `407.6µs` | `492.8µs` |
+| [Day 3](./src/bin/03.rs) | `11.8µs` | `15.6µs` |
 
-**Total: 0.86ms**
+**Total: 0.97ms**
 <!--- benchmarking table --->
 
 ---
--- src/bin/01.rs
+++ src/bin/01.rs
@@ -115,13 +115,13 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        let result: Option<u64> = part_one(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(514579));
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        let result: Option<u64> = part_two(&advent_of_code::template::read_file("examples", DAY));
         assert_eq!(result, Some(241861950));
     }
 }
