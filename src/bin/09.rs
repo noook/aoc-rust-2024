e37a0dddb963599193d694debd9cd5f4287bdc1e
noook <me@neilrichter.com>
2025-09-02 22:09:55 +0200

    feat: implement Day 9 solution and add example data

---
+++ src/bin/09.rs
@@ -0,0 +1,73 @@
+advent_of_code::solution!(9);
+
+fn parse_input(input: &str) -> Vec<u64> {
+    input
+        .trim()
+        .chars()
+        .map(|c| c.to_digit(10).unwrap() as u64)
+        .collect()
+}
+
+pub fn part_one(_input: &str) -> Option<u64> {
+    let input = parse_input(_input);
+    let mut chain = String::new();
+
+    let mut idx = 0;
+    let mut iter = input.iter();
+    while let Some(&file_len) = iter.next() {
+        let file_id = idx;
+        chain.push_str(&file_id.to_string().repeat(file_len as usize));
+        idx += 1;
+        if let Some(&space_len) = iter.next() {
+            chain.push_str(&".".repeat(space_len as usize));
+        }
+    }
+
+    let mut i = 0;
+    let mut j = chain.len() - 1;
+
+    let chars: Vec<char> = chain.chars().collect();
+
+    // Compact the disk and calculate checksum in one pass
+    let mut result = 0;
+    while i <= j {
+        if chars[i] != '.' {
+            // Element is in correct position
+            let file_id = chars[i].to_digit(10).unwrap() as u64;
+            result += i as u64 * file_id;
+            i += 1;
+        } else if chars[j] == '.' {
+            // Skip empty space from the right
+            j -= 1;
+        } else {
+            // Use element from j at position i
+            let file_id = chars[j].to_digit(10).unwrap() as u64;
+            result += i as u64 * file_id;
+            i += 1;
+            j -= 1;
+        }
+    }
+
+    Some(result)
+}
+
+pub fn part_two(_input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(1928));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, None);
+    }
+}
